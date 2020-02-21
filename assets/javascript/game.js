c38e1b4efc33ea9093a3128969b64e8bbdcf7493
Haylee McLemore <hayleemclemore@Haylees-MacBook-Pro.local>
2020-02-21 16:01:16 -0600

    adding javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,76 @@
 
+var maxGuesses = 13;
+var wins = 0;
+var letters = /^[A-Za-z]+$/;
+var letterGuessed = "";
+var wordGuessed = "";
 
-
-
-
-// var maxGuesses = 13;
-// var wins = 0;
-
-
-// var letters = /^[A-Za-z]+$/;
-// var letterGuessed = "";
-// var wordGuessed = "";
 
 
 $(document).ready(function() {
 
-var wordList = ["madonna", "computer", "oranges", "snow", "pizza", "sidewalk", "horse"];
-var numLetters = words [0];
 
-for (int i = 0; i < wordList.length(); i++)
-    if (Character.isLetter(s.charAt(i)))
-        document.write("_ " + i);
+     // This is our starting myFarm array.
+        var wordList = ["madonna", "computer", "oranges", "snow", "pizza", "sidewalk", "horse"];
+
+     // Creating a variable to hold our array length.
+        var wordListLength = wordList.length;
+
+     // Looping through our myFarm array.
+     for (var i = 0; i < wordListLength; i++) {
+
+       // Console out the farm animal in the current index.
+       console.log(wordListLength[i]);
+       var newStr = Array(wordList[i].length).join(' __')
+
+       // If the first character in the current animal is "c" or "o", alert the following message.
+    //    if (myFarm[j].charAt(0) === "c" || myFarm[j].charAt(0) === "o") {
+    //      alert("Starts with a c or an o!");
+    //    }
+
+     }
+
+    // for (var i = 0; i < wordList[i].length; i++){
+     
+
+    
+    // };
+
+    //Adding spaces for the current Word to game card 
+    var spaces = document.getElementById("currentWord");
+    var text = document.createTextNode(newStr);
+    spaces.appendChild(text);
+
+    //Displaying number of wins
 
 
 
-// for (var i = 0; i < words.length; i++) {
-//     document.write("_ " words);
+    var userText = document.getElementById("user-text");
 
-// }
+      // Next, we give JavaScript a function to execute when onkeyup event fires.
+      document.onkeyup = function(event) {
+        userText.textContent = event.key;
+      };
 
-// $("#currentWord").append("<br><hr>" + numLetters) 
+    var lettersGuessed = document.getElementById("lettersGuessed");
+
+      // Next, we give JavaScript a function to execute when onkeyup event fires.
+      document.onkeyup = function(event) {
+        lettersGuessed.textContent = event.key;
+      };
+
+
+      updateInformation ()
+
+
+      function updateInformation () {
+
+        var lettersGuessed = document.getElementById("lettersGuessed");
+
+      }
 });
 
 
-// function char_count(array, letter) 
-// {
-//  var letter_Count = 1;
-//  for (var position = 1; position < str.length; position++) 
-//  {
-//     if (str.charAt(position) == letter) 
-//       {
-//       letter_Count += 1;
-//       }
-//   }
-//   return letter_Count;
-// }
-
-// console.log(char_count('w3resource.com', 'o'));
-
-
-//   // Waiting for a key to be pressed
-//   document.onkeypress = function(event) {
-
-//     // Checking for the key to be an alphabetical character
-//     if (isAlpha(event.key) && !pauseGame) {
-//         checkForLetter(event.key.toUpperCase())
-//     }
-// }
-
-// $("#random-number").prepend("<br><hr>" + lotteryNum) 
 
 
 
-
-// };
