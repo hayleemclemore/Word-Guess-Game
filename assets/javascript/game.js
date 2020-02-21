977906b3d9a7876cff077833678e82ef8ffb0c34
Haylee McLemore <hayleemclemore@Haylees-MacBook-Pro.local>
2020-02-21 08:22:55 -0600

    html and cssfor word guess game

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+// var maxGuesses = 13;
+// var wins = 0;
+
+
+// var letters = /^[A-Za-z]+$/;
+// var letterGuessed = "";
+// var wordGuessed = "";
+
+
+$(document).ready(function() {
+
+var wordList = ["madonna", "computer", "oranges", "snow", "pizza", "sidewalk", "horse"];
+var numLetters = words [0];
+
+for (int i = 0; i < wordList.length(); i++)
+    if (Character.isLetter(s.charAt(i)))
+        document.write("_ " + i);
+
+
+
+// for (var i = 0; i < words.length; i++) {
+//     document.write("_ " words);
+
+// }
+
+// $("#currentWord").append("<br><hr>" + numLetters) 
+});
+
+
+// function char_count(array, letter) 
+// {
+//  var letter_Count = 1;
+//  for (var position = 1; position < str.length; position++) 
+//  {
+//     if (str.charAt(position) == letter) 
+//       {
+//       letter_Count += 1;
+//       }
+//   }
+//   return letter_Count;
+// }
+
+// console.log(char_count('w3resource.com', 'o'));
+
+
+//   // Waiting for a key to be pressed
+//   document.onkeypress = function(event) {
+
+//     // Checking for the key to be an alphabetical character
+//     if (isAlpha(event.key) && !pauseGame) {
+//         checkForLetter(event.key.toUpperCase())
+//     }
+// }
+
+// $("#random-number").prepend("<br><hr>" + lotteryNum) 
+
+
+
+
+// };
